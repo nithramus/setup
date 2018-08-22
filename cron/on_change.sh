daad1055771fe303e2821d217561041080b2abfb
root <batiste.andre53@hotmail.fr>
2018-08-23 00:54:34 +0200

    auto update2

--- cron/on_change.sh
+++ cron/on_change.sh
@@ -2,6 +2,6 @@ mkdir /tmp/yolo3
 cp /home/nithramir/.config/Code/User/keybindings.json /home/nithramir/setup/vscode/keybindings.js
 cd /home/nithramir/setup
 git add .
-git commit -m "auto update"
+git commit -m "auto update2"
 git push
 mkdir /tmp/yolo4
