04efe71a9ad1711e6603b40c6f4ab166608f0057
nithramir <batiste.andre53@hotmail.fr>
2018-08-22 02:40:55 +0200

    update

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -1,21 +1,23 @@
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get install git
-sudo apt-get install zsh
-sudo apt-get install vim
-sudo apt-get install curl
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y install git
+sudo apt-get -y install zsh
+sudo apt-get -y install vim
+sudo apt-get -y install curl
 curl -L http://install.ohmyz.sh | sh
 curl http://install.sublivim.com | sh || curl https://raw.githubusercontent.com/reversTeam/Sublivim/master/installer.sh | sh
-sudo apt-get install nodejs
+sudo apt-get -y install nodejs
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 sudo apt-get update
-sudo apt-get install code
+sudo apt-get -y install code
 md project
-sudo apt-get install google-chrome-stable
+sudo apt-get -y install google-chrome-stable
 sudo sh -c 'echo "deb [arch=amd64] https://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list'
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 sudo apt-get update
-sudo apt-get install google-chrome-stable
+sudo apt-get -y install google-chrome-stable
 sudo snap install discord
+sudo apt-get -y install npm
+sudo npm i -g nodemon
